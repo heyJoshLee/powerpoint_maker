b5dddfda167d9912a7e904c41734c7564694ccda
jleewebdev <lee45276@gmail.com>
2016-12-22 19:56:55 +0800

    Update Options

--- script.rb
+++ script.rb
@@ -2,16 +2,16 @@ require "HTTParty"
 require "pry"
 require "google_drive"
 require "yaml"
-# require 'active_support/core_ext/hash/conversions'
 require 'powerpoint'
 require 'open-uri'
 require "date"
 require 'fileutils'
 
 class PowerPointMaker
+  @@lessons = []
   attr_accessor :config, :pixabay_key, :ws, :time_stamp, :lesson_number,
                 :deck, :course_name, :lesson_name, :words, :vocabulary_sheet,
-                :quiz_questions, :session, :quiz_lesson_number, :lessons
+                :quiz_questions, :session, :quiz_lesson_number
 
 
   def self.prompt_user_for_inputs
@@ -86,25 +86,41 @@ class PowerPointMaker
 
   def self.create_multiple_presentations
     puts "Creating multiple presentations."
+
     puts "Put the course name to be used for all presentations."
     course_name_for_all_lessons = gets.chomp.strip
 
-    @lessons = []
-    
     counter = 1
     
     auto_generate = ""
     puts "Auto Generate Lesson info? Only do this if you know what you're doing. [y/n]"
     auto_generate = gets.chomp.downcase.strip
 
+    puts "What's the lesson type: Ex: Lesson, Episode, Chapter ('Lesson' is the default)"
+    lesson_type = gets.chomp
+    lesson_type = "Lesson" if lesson_type == ""
+
+
+    # Mass create ppt by using a range, or comma separated list of numbers
     if auto_generate == "y"
-      puts "Enter the lesson numbers as integers separated by commas. ex 1,2,5,9"
-      lessons_numbers_to_generate_automatically = gets.chomp.strip.split(",")
-      lessons_numbers_to_generate_automatically.each do |lesson_number|
-        @lessons << [course_name_for_all_lessons, "Lesson #{lesson_number}", (lesson_number.to_i * 100).to_s, lesson_number ]
+
+      puts "Enter the lesson numbers as integers separated by commas. ex 1,2,5,9 OR as a range [1-20]"
+      auto_generate_input = gets.chomp.strip
+
+      # parse range or comma separated lessons
+      if auto_generate_input.include?("-")
+        min_and_max = auto_generate_input.split("-").map! { |m| m.to_i }
+        lessons_numbers_to_generate_automatically = (min_and_max[0]..min_and_max[1]).to_a
+      else
+        lessons_numbers_to_generate_automatically = auto_generate_input.split(",")
       end
-      binding.pry
+
+      # Create lesson params and put into @@lessons
+      lessons_numbers_to_generate_automatically.each do |lesson_number|
+        @@lessons << [course_name_for_all_lessons, "#{lesson_type} #{lesson_number}", (lesson_number.to_i * 100).to_s, lesson_number.to_s ]
+      end
     else
+      # Continue to prompt use for Lesson info
       add_another_lesson = ""
       until add_another_lesson == "n"
         add_lesson_to_lessons_array(counter, course_name_for_all_lessons)
@@ -112,20 +128,21 @@ class PowerPointMaker
         add_another_lesson = gets.chomp.downcase
         counter += 1
       end
-
     end
 
+
+    # Generate all lessons
     puts "Start generating presentations."
-    @lessons.each.each_with_index do |l, index|
+    @@lessons.each.each_with_index do |l, index|
       puts "Creating lesson: #{l[1]}"
       PowerPointMaker.new(l[0], l[1], l[2], l[3])
       puts "Finished creating presentation for lesson."
-      puts "#{@lessons.count - (index + 1)} lessons left"
+      puts "#{@@lessons.count - (index + 1)} lessons left"
     end
-
   end
 
   def self.add_lesson_to_lessons_array(counter, course_name)
+    # Allow user to compose an array that contains info for all lessons to  be processed
     confirm_inputs = ""
     until confirm_inputs == "y"
 
@@ -148,7 +165,7 @@ class PowerPointMaker
       puts "[y/n]"
 
       confirm_inputs = gets.chomp.downcase
-      @lessons << presentation_inputs if confirm_inputs == "y"
+      @@lessons << presentation_inputs if confirm_inputs == "y"
     end
   end
 
@@ -217,7 +234,6 @@ class PowerPointMaker
     @quiz_sheet = @config["GOOGLE_SHEET_QUIZ"]
     OpenSSL::SSL.const_set(:VERIFY_PEER, OpenSSL::SSL::VERIFY_NONE)
     @session = GoogleDrive::Session.from_config("config.json")
-
   end
 
   def connect_to_vocabulary_workbook
@@ -225,7 +241,11 @@ class PowerPointMaker
   end
 
   def connect_to_quiz_workbook
-    @ws_2 = @session.spreadsheet_by_key(@quiz_sheet).worksheets[0]
+    begin
+      @ws_2 = @session.spreadsheet_by_key(@quiz_sheet).worksheets[0]
+    rescue Exception => error
+      @ws_2 = false
+    end
   end
 
   def set_output_directory
@@ -278,36 +298,39 @@ class PowerPointMaker
     @quiz_questions = []
     question = {}
     started = false
+    if @ws_2
+      (2..@ws_2.num_rows).each_with_index do |row, index|
+         unless started
+              started = @ws_2[row, 5] == @quiz_lesson_number
+            next unless started
+          end
+        
+        if @ws_2[row,1] == "question"
 
-    (2..@ws_2.num_rows).each_with_index do |row, index|
-       unless started
-            started = @ws_2[row, 5] == @quiz_lesson_number
-          next unless started
+         
+          @quiz_questions << question unless question.empty?
+
+
+          break if @ws_2[row, 5] != @quiz_lesson_number && @ws_2[row, 5] != ""
+
+          question = {
+            body: @ws_2[row, 3],
+            choices: []
+          }
+        else
+          choice = {
+            body: @ws_2[row, 3],
+            correct: (@ws_2[row, 4] == "TRUE" ? true : false)
+          }
+          question[:choices] << choice
         end
-      
-      if @ws_2[row,1] == "question"
-
-       
-        @quiz_questions << question unless question.empty?
-
-
-        break if @ws_2[row, 5] != @quiz_lesson_number && @ws_2[row, 5] != ""
-
-        question = {
-          body: @ws_2[row, 3],
-          choices: []
-        }
-      else
-        choice = {
-          body: @ws_2[row, 3],
-          correct: (@ws_2[row, 4] == "TRUE" ? true : false)
-        }
-        question[:choices] << choice
       end
     end
   end
 
   def create_slides_for_questions
+    # if there are no quiz questions, don't create slides for the quizzes
+    # create a question and answer slide for each question
     unless @quiz_questions.empty?
       @quiz_questions.each do |question|
         begin
